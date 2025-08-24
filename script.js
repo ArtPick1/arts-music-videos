c5110ae064fdac43a26408835634132b0fa14518
ArtPick1 <artprepper@gmail.com>
2025-08-23 20:22:37 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,155 +1,161 @@
 const videos = [
   {
-    title: "Faith Unchained",
+    title: "Yirat Ometz \u2013 Song of Reverent Boldness",
     videoUrl: "https://www.youtube.com/embed/_m7Ko6mzxd0",
-    description: "<p><strong>Faith Unchained</strong> is a gritty rock/metal anthem inspired by Philippians 1:12\u201314 and Matthew 28:18\u201320. \nPaul\u2019s imprisonment became a platform for the Gospel \u2014 and we\u2019re called to live the same way: bold, unashamed, unchained.\nThis song is a wake-up call to believers in a culture of fear and complacency. The Church is not called to sit silent or just bring people to a preacher \u2014 we are commissioned to GO, to proclaim Christ, to live unbound by fear.</p>\n<p><strong>\ud83d\udd17 Key Scriptures:</strong><br>\nPhilippians 1:12\u201314<br>\nMatthew 28:18\u201320</p>"
+    description: "\n\ud83d\udd25 <strong>Yirat-Ometz</strong> \u2014 Hebrew for \u201cReverent Boldness\u201d \u2014 is a cry for a generation to rise in holy fear and Spirit-empowered courage.\n\n<p><em>Inspired by Malachi 2:4\u20135 and Malachi 1:8</em>, this song confronts a sobering truth:</p>\n\n<p>The modern Church has lost its fear and dread of God.<br>\nWe\u2019ve lost reverence\u2026 and we\u2019ve lost our fire.</p>\n\n<p>We offer up polluted worship.<br>\nWe withhold our best, our all.<br>\nWe lay part of ourselves on the altar \u2014 but not everything.</p>\n\n<blockquote>\u201cWhen you offer blind animals in sacrifice, is that not evil? \u2026 Present that to your governor; will he accept you or show you favor?\u201d \u2013 Malachi 1:8</blockquote>\n\n<p>We talk of grace, but forget God\u2019s holiness.<br>\nWe speak of love, but neglect surrender.</p>\n\n<p>This isn\u2019t about louder music or better performances \u2014 it\u2019s about laying our lives down in trembling obedience.</p>\n\n<blockquote>\u201cMy covenant was with him, a covenant of life and peace\u2026 a covenant of fear, and he feared Me. He stood in awe of My name.\u201d \u2013 Malachi 2:5</blockquote>\n\n<p>This is not pride shouting \u2014 it\u2019s humility burning.<br>\n<strong>\ud83d\udd25 This is Yirat-Ometz \u2014 reverent boldness.</strong>\n</p>\n"
   },
   {
-    title: "Nothing Held Back",
-    videoUrl: "https://www.youtube.com/embed/be5UgHGGa4g",
-    description: "<p><strong>Nothing Held Back</strong> is a raw, unapologetic rock/metal anthem calling believers to full surrender to Christ...</p>"
+    title: "Song 1",
+    videoUrl: "https://www.youtube.com/embed/video1",
+    description: "Description for song 1"
   },
   {
-    title: "Come and Grow (Rap)",
-    videoUrl: "https://www.youtube.com/embed/buws9YQyytU",
-    description: "<p><strong>Come and Grow (Rap)</strong> hits hard with raw bars, heavy beats, and a straight-up call to truth...</p>"
+    title: "Song 2",
+    videoUrl: "https://www.youtube.com/embed/video2",
+    description: "Description for song 2"
   },
   {
-    title: "Are You Living a Lie?",
-    videoUrl: "https://www.youtube.com/embed/-SrZNafvoio",
-    description: "<p>...</p>"
+    title: "Song 3",
+    videoUrl: "https://www.youtube.com/embed/video3",
+    description: "Description for song 3"
   },
   {
-    title: "Not Just Another Pretty Voice",
-    videoUrl: "https://www.youtube.com/embed/Imp4sM4-7g0",
-    description: "<p>...</p>"
+    title: "Song 4",
+    videoUrl: "https://www.youtube.com/embed/video4",
+    description: "Description for song 4"
   },
   {
-    title: "The Temple of the Living God",
-    videoUrl: "https://www.youtube.com/embed/93E1lOJSq6I",
-    description: "<p>...</p>"
+    title: "Song 5",
+    videoUrl: "https://www.youtube.com/embed/video5",
+    description: "Description for song 5"
   },
   {
-    title: "Why Go to Church?",
-    videoUrl: "https://www.youtube.com/embed/KIF8zRQWsis",
-    description: "<p>...</p>"
+    title: "Song 6",
+    videoUrl: "https://www.youtube.com/embed/video6",
+    description: "Description for song 6"
   },
   {
-    title: "Come and Grow",
-    videoUrl: "https://www.youtube.com/embed/8JGBLmRi14M",
-    description: "<p>...</p>"
+    title: "Song 7",
+    videoUrl: "https://www.youtube.com/embed/video7",
+    description: "Description for song 7"
   },
   {
-    title: "Come and Grow Pencil Drawings Blues RB",
-    videoUrl: "https://www.youtube.com/embed/skXVC7SjMIg",
-    description: "<p>...</p>"
+    title: "Song 8",
+    videoUrl: "https://www.youtube.com/embed/video8",
+    description: "Description for song 8"
   },
   {
-    title: "Jesus and Bocephus",
-    videoUrl: "https://www.youtube.com/embed/9OQmrhu7UPY",
-    description: "<p>...</p>"
+    title: "Song 9",
+    videoUrl: "https://www.youtube.com/embed/video9",
+    description: "Description for song 9"
   },
   {
-    title: "We Can\u2019t Let Go",
-    videoUrl: "https://www.youtube.com/embed/0lBXQ7u8Z6g",
-    description: "<p>...</p>"
+    title: "Song 10",
+    videoUrl: "https://www.youtube.com/embed/video10",
+    description: "Description for song 10"
   },
   {
-    title: "Depths Of My Distress",
-    videoUrl: "https://www.youtube.com/embed/Rh69t81Co9Q",
-    description: "<p>...</p>"
+    title: "Song 11",
+    videoUrl: "https://www.youtube.com/embed/video11",
+    description: "Description for song 11"
   },
   {
-    title: "His Holy Call",
-    videoUrl: "https://www.youtube.com/embed/GyccPooWTrc",
-    description: "<p>...</p>"
+    title: "Song 12",
+    videoUrl: "https://www.youtube.com/embed/video12",
+    description: "Description for song 12"
   },
   {
-    title: "A New Creation (Behold\u2014the New Has Come)",
-    videoUrl: "https://www.youtube.com/embed/BZKlUIWSv4E",
-    description: "<p>...</p>"
+    title: "Song 13",
+    videoUrl: "https://www.youtube.com/embed/video13",
+    description: "Description for song 13"
   },
   {
-    title: "Are You Truly Born Again Rock",
-    videoUrl: "https://www.youtube.com/embed/yK4YxAU5e_E",
-    description: "<p>...</p>"
+    title: "Song 14",
+    videoUrl: "https://www.youtube.com/embed/video14",
+    description: "Description for song 14"
   },
   {
-    title: "Are You Truly Born Again Metal",
-    videoUrl: "https://www.youtube.com/embed/2DjXCaqsNpk",
-    description: "<p>...</p>"
+    title: "Song 15",
+    videoUrl: "https://www.youtube.com/embed/video15",
+    description: "Description for song 15"
   },
   {
-    title: "Sing Hallelujah",
-    videoUrl: "https://www.youtube.com/embed/HZYPymgsTNU",
-    description: "<p>...</p>"
+    title: "Song 16",
+    videoUrl: "https://www.youtube.com/embed/video16",
+    description: "Description for song 16"
   },
   {
-    title: "Why Do We Go After Worthless Idols",
-    videoUrl: "https://www.youtube.com/embed/6DSL94G6Tko",
-    description: "<p>...</p>"
+    title: "Song 17",
+    videoUrl: "https://www.youtube.com/embed/video17",
+    description: "Description for song 17"
   },
   {
-    title: "This Ain\u2019t for Awards\u2014It\u2019s for War",
-    videoUrl: "https://www.youtube.com/embed/QNN2oaTuWMM",
-    description: "<p>...</p>"
+    title: "Song 18",
+    videoUrl: "https://www.youtube.com/embed/video18",
+    description: "Description for song 18"
   },
   {
-    title: "Yet I Will Rejoice",
-    videoUrl: "https://www.youtube.com/embed/NfZtq1BN5LU",
-    description: "<p>...</p>"
+    title: "Song 19",
+    videoUrl: "https://www.youtube.com/embed/video19",
+    description: "Description for song 19"
   },
   {
-    title: "Does God Revolve Around You?",
-    videoUrl: "https://www.youtube.com/embed/Q4_JYGVL8Wo",
-    description: "<p>...</p>"
+    title: "Song 20",
+    videoUrl: "https://www.youtube.com/embed/video20",
+    description: "Description for song 20"
   },
   {
-    title: "Dust to Dust",
-    videoUrl: "https://www.youtube.com/embed/goYC-AN2Rzs",
-    description: "<p>...</p>"
+    title: "Song 21",
+    videoUrl: "https://www.youtube.com/embed/video21",
+    description: "Description for song 21"
   },
   {
-    title: "Disturb the City (Version 2)",
-    videoUrl: "https://www.youtube.com/embed/jGJYsMzGUls",
-    description: "<p>...</p>"
+    title: "Song 22",
+    videoUrl: "https://www.youtube.com/embed/video22",
+    description: "Description for song 22"
   },
   {
-    title: "Disturb the City (Original)",
-    videoUrl: "https://www.youtube.com/embed/QLz4KNQmrAo",
-    description: "<p>...</p>"
+    title: "Song 23",
+    videoUrl: "https://www.youtube.com/embed/video23",
+    description: "Description for song 23"
   },
   {
-    title: "Disciple or Fan",
-    videoUrl: "https://www.youtube.com/embed/FulLRuDHai0",
-    description: "<p>...</p>"
+    title: "Song 24",
+    videoUrl: "https://www.youtube.com/embed/video24",
+    description: "Description for song 24"
   },
   {
-    title: "From Where I Stand",
-    videoUrl: "https://www.youtube.com/embed/eOtEThVwtTs",
-    description: "<p>...</p>"
+    title: "Song 25",
+    videoUrl: "https://www.youtube.com/embed/video25",
+    description: "Description for song 25"
   },
   {
-    title: "Accident or Design",
-    videoUrl: "https://www.youtube.com/embed/kJU5JDUgItY",
-    description: "<p>...</p>"
+    title: "Song 26",
+    videoUrl: "https://www.youtube.com/embed/video26",
+    description: "Description for song 26"
   },
   {
-    title: "When Minds Meet Machine",
-    videoUrl: "https://www.youtube.com/embed/Q7cg_SLXDm4",
-    description: "<p>...</p>"
+    title: "Song 27",
+    videoUrl: "https://www.youtube.com/embed/video27",
+    description: "Description for song 27"
   },
   {
-    title: "Strength in Surrender",
-    videoUrl: "https://www.youtube.com/embed/ZHfX1J60YcA",
-    description: "<p>...</p>"
+    title: "Song 28",
+    videoUrl: "https://www.youtube.com/embed/video28",
+    description: "Description for song 28"
   },
   {
-    title: "The Cry Unheard",
-    videoUrl: "https://www.youtube.com/embed/EAoTEsSZGVE",
-    description: "<p>...</p>"
+    title: "Song 29",
+    videoUrl: "https://www.youtube.com/embed/video29",
+    description: "Description for song 29"
+  },
+  {
+    title: "Song 30",
+    videoUrl: "https://www.youtube.com/embed/video30",
+    description: "Description for song 30"
   }
 ];
+
 
 function renderVideos() {
   const container = document.getElementById("video-grid");
