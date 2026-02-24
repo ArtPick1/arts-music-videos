499571d23b47ae9fe4923023d66653b904afbf55
ArtPick1 <artprepper@gmail.com>
2026-02-24 13:11:15 -0600

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,14 @@
+// vHN2602241751
+window.VIDEOS = [
+  {
+    "id": "holy-awe-you-alone-are-worthy",
+    "title": "HOLY AWE You Alone Are Worthy",
+    "youtubeId": "9psk-bVUTbY",
+    "published": "",
+    "tags": [
+      "Featured"
+    ],
+    "message": "Featured video"
+  }
+];
+window.FEATURED_VIDEO_ID = "holy-awe-you-alone-are-worthy";
